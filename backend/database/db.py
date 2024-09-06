cf2f061857a770a1de1b8b90579980acc1d12cc9
haremir <harunemirhan826@gmail.com>
2024-09-06 22:05:43 +0300

    database folders added on the backend

---
+++ backend/database/db.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  6 21:52:38 2024
+
+@author: emirh
+"""
+
---
+++ backend/database/models.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  6 21:52:50 2024
+
+@author: emirh
+"""
+
